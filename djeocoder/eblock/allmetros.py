6e4a9d8beab80ffbf069c4f2fb749ca77518401a
Timothy Danford <tdanford@gmail.com>
2010-11-16 17:29:42 -0500

    Removed the eblock/ directory.

--- djeocoder/eblock/allmetros.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-
-METRO_LIST = settings.METRO_LIST
-METRO_DICT = dict([(m['short_name'], m) for m in METRO_LIST])
-
-def get_metro(short_name=None):
-    if short_name is None:
-        short_name = settings.SHORT_NAME
-    return METRO_DICT[short_name]
--- djeocoder/eblock/openblock.py
+++
@@ -1,7 +0,0 @@
-import ebpub.gecoder.base.Geocoder
-
-class OpenblockGeocoder(ebpub.geocoder.base.Geocoder):
-	def __init(self, gc):
-		self.geocoder=gc
-	def _do_geocode(self, location_string):
-		return self.geocoder.geocode(location_string)
